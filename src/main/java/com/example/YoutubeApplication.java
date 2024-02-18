c2a5b016f7367d8ad94819ce882f2d2b5729c04b
Aslbek Norqulov <aslbeknorqulov246@gmail.com>
2024-02-18 15:54:30 +0500

    youtube project started

---
+++ src/main/java/com/example/YoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeApplication.class, args);
+    }
+
+}
