a4adb68b047f20e69caa7a625034dbf7e66a25f8
Aslbek Norqulov <aslbeknorqulov246@gmail.com>
2024-02-20 19:21:05 +0500

    auth login api added

--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
+import com.example.dto.AuthDTO;
 import com.example.dto.RegistrationDTO;
 import com.example.enums.AppLanguage;
 import com.example.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,7 @@ public class AuthController {
     @Operation(summary = "Api for registration", description = "this api used for authorization")
     public ResponseEntity<?> registration(@RequestBody RegistrationDTO dto,
                                           @RequestHeader(value = "Accept-Language", defaultValue = "UZ") AppLanguage appLanguage) {
+        log.info("registration {}",dto.getEmail());
         return ResponseEntity.ok(authService.registration(dto, appLanguage));
     }
 
@@ -31,6 +34,14 @@ public class AuthController {
                                           @RequestHeader(value = "Accept-Language", defaultValue = "UZ") AppLanguage language) {
         log.info("emailVerification {}", jwt);
         return ResponseEntity.ok(authService.verification(jwt, language));
+    }
+
+    @Operation(summary = "Api for login",description = "this api used for login")
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthDTO dto,
+                                   @RequestHeader(value = "Accept-Language",defaultValue = "UZ") AppLanguage language){
+        log.info("login {}",dto.getEmail());
+        return ResponseEntity.ok(authService.login(dto,language));
     }
 
 }
