46627b9e85ac04b7af3f8cbde0379efb6abdf0d6
ShamshodOdinayev <shamshododinayev1702@gmail.com>
2024-02-24 18:10:44 +0500

    update

--- src/main/java/com/example/controller/VideoController.java
+++ src/main/java/com/example/controller/VideoController.java
@@ -5,14 +5,12 @@ import com.example.dto.UpdateStatusVideoDTO;
 import com.example.dto.VideoCreateDTO;
 import com.example.dto.VideoDTO;
 import com.example.enums.AppLanguage;
-import com.example.enums.VideoStatus;
 import com.example.service.VideoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -26,8 +24,8 @@ public class VideoController {
 
     @PostMapping("/any")
     @Operation(summary = "This api Video Create", description = "This api is used to create video")
-    public ResponseEntity<VideoCreateDTO> create(@RequestBody VideoCreateDTO dto,
-                                                 @RequestHeader(value = "Accept-Language", defaultValue = "UZ") AppLanguage language) {
+    public ResponseEntity<VideoDTO> create(@RequestBody VideoCreateDTO dto,
+                                           @RequestHeader(value = "Accept-Language", defaultValue = "UZ") AppLanguage language) {
         log.info("There is an error in what you sent {}", dto);
         return ResponseEntity.ok(videoService.create(dto, language));
     }
@@ -38,6 +36,7 @@ public class VideoController {
                                                 @RequestHeader(value = "Accept-Language", defaultValue = "UZ") AppLanguage language) {
         return ResponseEntity.ok(videoService.updateDetail(dto, videoId, language));
     }
+
     @PutMapping("/any/updateStatus")
     @Operation(summary = "This api Video Create", description = "This api is used to create video")
     public ResponseEntity<?> updateStatus(@RequestBody UpdateStatusVideoDTO dto,
