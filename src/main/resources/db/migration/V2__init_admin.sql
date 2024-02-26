98443ff303ad8a6f80bdb58d5bda52073991bc5c
Aslbek Norqulov <aslbeknorqulov246@gmail.com>
2024-02-26 18:44:13 +0500

    flyway update

--- src/main/resources/db/migration/V2__init_admin.sql
+++ src/main/resources/db/migration/V2__init_admin.sql
@@ -1,3 +1,3 @@
-insert into profile (name, surname, email, password, status, role, photo_id, visible, created_date)
-values ('admin', 'adminjon', 'adminjon@gmail.com', '0192023a7bbd73250516f069df18b500', 'ACTIVE', 'ROLE_ADMIN', '68e685f5-5e49-4ae9-9a14-1bc65746220a', true,
+insert into profile (name, surname, email, password, status, role, visible, created_date)
+values ('admin', 'adminjon', 'adminjon@gmail.com', '0192023a7bbd73250516f069df18b500', 'ACTIVE', 'ROLE_ADMIN', true,
         current_timestamp);
