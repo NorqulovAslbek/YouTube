a55b026734c9871ad8723b1c460310fc01fd446f
ShamshodOdinayev <shamshododinayev1702@gmail.com>
2024-02-28 15:48:18 +0500

    playlist native query add

--- src/main/resources/db/migration/V2__init_admin.sql
+++ src/main/resources/db/migration/V2__init_admin.sql
@@ -1,3 +1,4 @@
-insert into profile (name, surname, email, password, status, role, photo, visible, created_date)
-values ('admin', 'adminjon', 'adminjon@gmail.com', '0192023a7bbd73250516f069df18b500', 'ACTIVE', 'ROLE_ADMIN', 'gg', true,
+insert into profile (name, surname, email, password, status, role, visible, created_date)
+values ('admin', 'adminjon', 'adminjon@gmail.com', '0192023a7bbd73250516f069df18b500', 'ACTIVE', 'ROLE_ADMIN',
+        true,
         current_timestamp);
