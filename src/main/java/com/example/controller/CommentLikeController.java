6f3366a3057bcac3f8d8ac830aec74a9eec9a206
ahror <ahrororzimurodov@icloud.com>
2024-02-29 20:13:22 +0500

    comment like all API added

--- src/main/java/com/example/controller/CommentLikeController.java
+++ src/main/java/com/example/controller/CommentLikeController.java
@@ -1,13 +1,18 @@
 package com.example.controller;
 
+import com.example.dto.CommentLikeDTO;
 import com.example.dto.CreateCommentLikeDTO;
+import com.example.entity.CommentEntity;
 import com.example.enums.AppLanguage;
 import com.example.service.CommentLikeService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comment-like")
@@ -20,6 +25,34 @@ public class CommentLikeController {
     public ResponseEntity<?> create(@Valid @RequestBody CreateCommentLikeDTO dto){
         return ResponseEntity.ok(commentLikeService.create(dto));
     }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Api for remove comment like ")
+    public ResponseEntity<?> remove(@PathVariable("id") Integer id,
+                                    @RequestHeader(value = "Accept-Language", defaultValue = "UZ")
+                                    AppLanguage language){
+        return ResponseEntity.ok(commentLikeService.remove(id,language));
+
+    }
+    @GetMapping("")
+    @Operation(summary = "Api for get  user liked comment list ")
+    public ResponseEntity<List<CommentLikeDTO>> getLikedList(  @RequestHeader(value = "Accept-Language", defaultValue = "UZ")
+                                                                   AppLanguage language){
+        return ResponseEntity.ok(commentLikeService.getLikedList(language));
+    }
+
+    @GetMapping("/{profileId}")
+    @Operation(summary = "Api for get get user likedComment list by userId for ADMIN")
+    @PreAuthorize(value = "hasRole('ADMIN')")
+    public ResponseEntity<List<CommentLikeDTO>>  getUserLikedCommentListByUserId(@PathVariable("profileId") Integer profileId,
+                                                                                @RequestHeader(value = "Accept-Language", defaultValue = "UZ")
+                                                                                AppLanguage language){
+        return ResponseEntity.ok(commentLikeService.getUserLikedCommentListByUserId(profileId,language));
+    }
+
+
+
+
 
 
 
--- src/main/java/com/example/service/CommentLikeService.java
+++ src/main/java/com/example/service/CommentLikeService.java
@@ -3,18 +3,25 @@ package com.example.service;
 import com.example.dto.CommentLikeDTO;
 import com.example.dto.CreateCommentLikeDTO;
 import com.example.entity.CommentLikeEntity;
+import com.example.enums.AppLanguage;
+import com.example.exp.AppBadException;
 import com.example.repository.CommentLikeRepository;
 import com.example.util.SpringSecurityUtil;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CommentLikeService {
     @Autowired
     private CommentLikeRepository commentLikeRepository;
+    @Autowired
+    private ResourceBundleService resourceBundleService;
 
     public Object create(CreateCommentLikeDTO dto) {
         Integer profileId = SpringSecurityUtil.getCurrentUser().getId();
@@ -48,5 +55,41 @@ public class CommentLikeService {
         dto.setProfileId(entity.getProfileId());
         dto.setUpdatedDate(entity.getUpdatedDate());
         return dto;
+    }
+
+    public Object remove(Integer id, AppLanguage language) {
+        CommentLikeEntity entity = get(id,language);
+        commentLikeRepository.delete(entity);
+        return true;
+    }
+
+    public CommentLikeEntity get(Integer commentLikeId, AppLanguage language) {
+        Optional<CommentLikeEntity> optionalVideoEntity = commentLikeRepository.findById(commentLikeId);
+        if (optionalVideoEntity.isEmpty()) {
+            throw new AppBadException(resourceBundleService.getMessage("comment.like.not.found", language));
+        }
+        return optionalVideoEntity.get();
+    }
+
+    public List<CommentLikeDTO> getLikedList(AppLanguage language) {
+        Integer profileId = SpringSecurityUtil.getCurrentUser().getId();
+        return getCommentLikeDTOS(profileId, language);
+    }
+
+    public List<CommentLikeDTO> getUserLikedCommentListByUserId(Integer profileId,AppLanguage language) {
+        return getCommentLikeDTOS(profileId, language);
+    }
+
+    @NotNull
+    private List<CommentLikeDTO> getCommentLikeDTOS(Integer profileId, AppLanguage language) {
+        List<CommentLikeEntity> list = commentLikeRepository.getList(profileId);
+        if (list.isEmpty()){
+            throw new AppBadException(resourceBundleService.getMessage("comment.like.not.found",language));
+        }
+        List<CommentLikeDTO> dtoList = new ArrayList<>();
+        for (CommentLikeEntity entity:list){
+            dtoList.add(toDTO(entity));
+        }
+        return dtoList;
     }
 }
